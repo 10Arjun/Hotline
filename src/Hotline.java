f3421c63225b115f5b2af957d36809a86153a8ed
Iyera-rgb <arjun.iyer.20@gmail.com>
2022-08-28 17:32:24 +0530

    LOL

--- src/Hotline.java
+++ src/Hotline.java
@@ -9,39 +9,42 @@ class Hotline {
 
     static void ASCIIConvert(String str) {
         int l = str.length();
-        int convertASCII;
+        int convertASCII = 0;
         char[] ch = str.toCharArray();
         String newStr = "";
         for (int i = 0; i < 7; i++) {
             convertASCII = str.charAt(i);
             System.out.print(convertASCII);
             newStr += ch[i];
-       }
+        }
     }
 
 
     // Driver function
     public static void main(String[] args) {
         boolean Running = true;
+        while (Running == true) {
             Scanner sc = new Scanner(System.in);
             System.out.println("Please input the hotline you would like to contact");
-            String Conversion = sc.next();
+            String Conversion = sc.nextLine();
+            //PARSE NUMBERS
             char[] Temporary = Conversion.toCharArray();
+            Conversion = Conversion.toUpperCase();
 
             String siu = "";
 
-            for (int g = 0; g < Temporary.length; g++){
-                if (Temporary[g] != ' ')
-                {
+            for (int g = 0; g < Temporary.length; g++) {
+                if (Temporary[g] != ' ') {
                     siu = siu + Temporary[g];
                 }
 
             }
-            System.out.println(siu);
-//            String str = Conversion;
-//            Conversion = Conversion.toUpperCase();
-//            System.out.print("ASCII Sentence: 1-800-");
-//            ASCIIConvert(Conversion);
-//            System.out.println();
+            String str = Conversion;
+            System.out.print("ASCII Sentence: 1-800-");
+            ASCIIConvert(Conversion);
+            String Part1 = Conversion.substring
+            System.out.println();
         }
     }
+}
+
